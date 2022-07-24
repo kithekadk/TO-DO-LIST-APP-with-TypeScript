6c83e6c003d8a763c057fe32412a10c4e678a9a5
unknown <dankinyi99@gmail.com>
2022-07-24 23:54:03 +0300

    to do list ts

--- index.js
+++ index.js
@@ -4,6 +4,14 @@ window.addEventListener('load', () => {
     const task = document.querySelector(".task");
     const description = document.querySelector(".description");
     const due = document.querySelector(".deadline");
+    class uncompletedTodo {
+        constructor(taskTitle, taskDescription, deadline) {
+            this.taskTitle = taskTitle;
+            this.taskDescription = taskDescription;
+            this.deadline = deadline;
+        }
+    }
+    const pendingTasklist = [];
     formContainer.addEventListener("submit", (e) => {
         e.preventDefault();
         let datainputs = task.value && description.value && due.value;
@@ -11,22 +19,26 @@ window.addEventListener('load', () => {
             console.log("some task details missing");
         }
         else {
-            class uncompletedTodo {
-                constructor(taskTitle = `task Title:${task.value}`, taskDescription = `Task description ${description.value}`, deadline = `Deadline: ${due.value}`) {
-                    this.taskTitle = taskTitle;
-                    this.taskDescription = taskDescription;
-                    this.deadline = deadline;
-                }
-                uncompleted() {
-                    return this.taskTitle,
-                        this.taskDescription,
-                        this.deadline;
-                }
-            }
-            const newTask = new uncompletedTodo(`${task.value}`, `${description.value}`, `${due.value}`);
-            const pendingTasklist = [];
-            pendingTasklist.push(newTask);
-            console.log(pendingTasklist);
+            pendingTasklist.push(new uncompletedTodo(`${task.value}`, `${description.value}`, `${due.value}`));
+            // console.log(pendingTasklist)
+            Pending();
+            task.value = "";
+            description.value = "";
+            due.value = "";
         }
     });
+    function Pending() {
+        const duty = document.createElement("p");
+        duty.textContent = task.value;
+        const descp = document.createElement("p");
+        descp.textContent = description.value;
+        const duep = document.createElement("p");
+        duep.textContent = due.value;
+        const taskDetails = document.createElement("div");
+        taskDetails.appendChild(duty);
+        taskDetails.appendChild(descp);
+        taskDetails.appendChild(duep);
+        let uncompletedDiv = document.querySelector(".uncompletedDiv");
+        uncompletedDiv.appendChild(taskDetails);
+    }
 });
--- index.ts
+++ index.ts
@@ -5,38 +5,64 @@ const task =document.querySelector<HTMLInputElement>(".task");
 const description =document.querySelector<HTMLInputElement>(".description");
 const due =document.querySelector<HTMLInputElement>(".deadline");
 
+// uncompletedTodo class
+interface common  {
+    taskTitle: string,
+    taskDescription: string,
+    deadline: string,
+}
+class uncompletedTodo implements common{
+    constructor (
+        public taskTitle: string ,
+        public taskDescription: string ,
+        public deadline: string ,
+        ){}
+     
+}
+const pendingTasklist: uncompletedTodo[] = [];
+
+
 formContainer.addEventListener("submit",(e) =>{
     e.preventDefault();
     
     let datainputs= task.value && description.value && due.value ;
         if (!datainputs){
             console.log("some task details missing");
+
         } else{
-
-            class uncompletedTodo {
-                constructor (
-                    public taskTitle: string = `task Title:${task.value}`,
-                    public taskDescription: string = `Task description ${description.value}`,
-                    public deadline: string = `Deadline: ${due.value}`,
-                    ){}
-                
-                uncompleted(){
-                    return this.taskTitle, 
-                    this.taskDescription,
-                    this.deadline;
-                }  
-            }
-
-            const newTask = new uncompletedTodo (`${task.value}`, 
-                                                `${description.value}`,
-                                                `${due.value}`)
-
-            const pendingTasklist: uncompletedTodo[] = [];
-            pendingTasklist.push(newTask);
-            console.log(pendingTasklist);
             
-
-       }
+            pendingTasklist.push(new uncompletedTodo (
+                `${task.value}`, 
+                `${description.value}`,
+                `${due.value}`));
+            // console.log(pendingTasklist)
+            Pending()                    
+            task.value="";
+            description.value="";
+            due.value="";
+              
+        }
+        
 })
 
+function Pending(){
+    const duty= document.createElement("p");
+    duty.textContent=task.value;
+
+    const descp= document.createElement("p");
+    descp.textContent=description.value;
+
+    const duep= document.createElement("p");
+    duep.textContent=due.value;
+
+    const taskDetails =document.createElement("div");
+
+    taskDetails.appendChild(duty);
+    taskDetails.appendChild(descp);
+    taskDetails.appendChild(duep);
+
+    let uncompletedDiv= document.querySelector(".uncompletedDiv")
+    uncompletedDiv.appendChild(taskDetails)
+}
 })
+
